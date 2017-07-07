483003837d19fee7b34091313f4184ffaf060a33
Ryan Sommers <ryansommers@gmail.com>
2017-07-07 15:29:09 -0400

    Initial setup of main endpoint

---
+++ app.py
@@ -0,0 +1,21 @@
+#!flask/bin/python
+import pymongo
+from flask import Flask, jsonify
+from config import MONGO_URI, DB_NAME, COLL_NAME
+
+app = Flask(__name__)
+
+client = pymongo.MongoClient(MONGO_URI)
+db = client.get_database(DB_NAME)
+coll = db.get_collection(COLL_NAME)
+
+@app.route('/CAB/api/v1.0/search/<str:queryString>', methods=['GET'])
+def search(queryString):
+	
+	cur = coll.find({"$text" : {"$search" : queryString }})
+	results = list(cur)
+	count = len(results)
+	return jsonify({'count':count, 'results':results})
+	
+if __name__ == '__main__':
+	app.run(debug=True)
