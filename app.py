ac6071d52e53d35ab775e8283d5b5c2615a1884b
Ryan Sommers <ryansommers@gmail.com>
2017-07-09 20:01:44 -0400

    Added endpoint for retrieving all decisions

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ client = pymongo.MongoClient(MONGO_URI)
 db = client.get_database(DB_NAME)
 coll = db.get_collection(COLL_NAME)
 
+@app.route('/CAB/api/v1.0/decisions/', methods=['GET'])
+def getAll()
+	
+	cur = coll.find({},{'_id':0})
+	results = list(cur)
+	count = len(results)
+	return jsonify({'count':count, 'results':results})
+	
 @app.route('/CAB/api/v1.0/search/<queryString>', methods=['GET'])
 def search(queryString):
 	
